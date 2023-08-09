e0993a34d26d53035e7cb4edd3ae44326d0456f7
sachinacharya94 <sachinacharya901@gmail.com>
2023-08-09 08:13:44 +0545

    Css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1 style={{
+        backgroundColor:"red",
+        color:"black",
+        padding: "20px",
+
+        }}>Hello World!</h1>
+        <h2 className='heading'>
+          Hello World2
+        </h2>
     </div>
   );
 }
